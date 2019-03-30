1432386f6a8c3fe69403a94edbf9fa7e0f321ae9
simplegr33n <gcgolda@gmail.com>
2019-03-30 17:34:47 -0600

    added vol control to player

--- src/components/music-player/TopBarPlayer.js
+++ src/components/music-player/TopBarPlayer.js
@@ -7,17 +7,25 @@ class TopBarPlayer extends Component {
 		super(props);
 		this.state = {
 			song: null,
-			fillPosition: "0%"
+			musicPosition: "0%",
+			volumePosition: "100%"
 		};
 
 		this.audioPlayer = new Audio();
 		this.audioPlayer.loop = false;
 		this.audioPlayer.addEventListener('timeupdate', () => {
 			var position = this.audioPlayer.currentTime / this.audioPlayer.duration;
-			this.setState({ fillPosition: position * 100 + '%' });
+			this.setState({ musicPosition: (position * 100) + '%' });
 		});
+		this.audioPlayer.addEventListener('volumechange', () => {
+			var position = this.audioPlayer.volume;
+			console.log("volume!" + position)
+			this.setState({ volumePosition: (position * 100) + '%' });
+		});
+		
 
 		this.seekBar = React.createRef()
+		this.volumeBar = React.createRef()
 	}
 
 	componentWillUpdate(props) {
@@ -54,27 +62,47 @@ class TopBarPlayer extends Component {
 		this.audioPlayer.currentTime = (this.audioPlayer.duration * seekRatio)
 	}
 
+	handleVolumeBarClick = (e) => {
+		let volumeRatio = 1 - ((e.pageY - e.target.offsetTop) / this.volumeBar.current.offsetHeight)
+		if (volumeRatio <= 0.05) {
+			volumeRatio = 0;
+		} else if (volumeRatio >= 0.95) {
+			volumeRatio = 1;
+		}
+
+		console.log(`hero... ${(e.pageY - e.target.offsetTop) / this.volumeBar.current.offsetHeight}`)
+		console.log(`2... ${1- ((e.pageY - e.target.offsetTop) / this.volumeBar.current.offsetHeight)}`)
+
+		this.audioPlayer.volume = volumeRatio;
+	}
+
 	render() {
 		if (this.state.song !== null) {
 			return (
 				<div id="Top-Bar-Player">
-					<div id="Top-Player-Info-Div">
-						<div id="Top-Player-Artist">
-							{this.state.song.artist}
-						</div>
-						&nbsp;-&nbsp;
+					<div id="Top-Player-Center-Div">
+						<div id="Top-Player-Info-Div">
+							<div id="Top-Player-Artist">
+								{this.state.song.artist}
+							</div>
+							&nbsp;-&nbsp;
 						<div id="Top-Player-Song">
-							{this.state.song.songName}
+								{this.state.song.songName}
+							</div>
+						</div>
+						<div id="seek-bar" ref={this.seekBar} onClick={this.handleSeekBarClick}>
+							<div id="fill" style={{ width: this.state.musicPosition }}></div>
+							<div id="handle"></div>
+						</div>
+						<div id="Top-Player-Btns">
+							<button>&#9198; </button>
+							<button id="TP-Play-Pause-Btn" onClick={this.handlePlayOrPause}>&#9654; / &#9208;</button>
+							<button>&#9197;</button>
 						</div>
 					</div>
-					<div id="seek-bar" ref={this.seekBar} onClick={this.handleSeekBarClick}>
-						<div id="fill" style={{ width: this.state.fillPosition }}></div>
-						<div id="handle"></div>
-					</div>
-					<div id="Top-Player-Btns">
-						<button>&#9198; </button>
-						<button id="TP-Play-Pause-Btn" onClick={this.handlePlayOrPause}>&#9654; / &#9208;</button>
-						<button>&#9197;</button>
+					<div id="volume-bar" ref={this.volumeBar} onClick={this.handleVolumeBarClick}>
+							<div id="volume-fill" style={{ height: this.state.volumePosition }}></div>
+							<div id="volume-handle"></div>
 					</div>
 				</div>
 			);
